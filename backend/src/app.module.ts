59d34fb900098ecca6146904bee99125f69eddbc
Daniel <jamoja64749@gmail.com>
2026-01-28 21:52:12 +0100

    chore(app): import auth, database and users module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DatabaseService } from './database/database.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, DatabaseModule],
+  providers: [DatabaseService],
 })
 export class AppModule {}
